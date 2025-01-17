e375e31c322d2cea6c8c2410354d42cf38d32b42
DARVINMS <darvin9787m@gmail.com>
2025-01-17 16:04:56 +0530

    code written in file3.php

--- file3.php
+++ file3.php
@@ -1,1 +1,32 @@
 
+<?php
+class school {
+    public $collage_name;
+    public $location;
+    public function __construct()
+    {
+        $this->collage_name= "knackforge";
+        $this->location="chennai";
+        
+    }
+}
+
+class department extends school{
+    public  $department_name;
+     public function __construct($deptname)
+     {
+        parent::__construct();
+       $this->department_name= $deptname;
+     }
+     public function __destruct()
+     {
+        echo "Thank you,\ndepartment name : {$this->department_name}\ncollage name : {$this->collage_name}\nlocation : {$this->location}";
+     }
+
+}
+
+$darvin = new department("iclasspro");
+
+echo $darvin->department_name,"\n";
+echo $darvin->collage_name,"\n";
+echo $darvin->location,"\n";
